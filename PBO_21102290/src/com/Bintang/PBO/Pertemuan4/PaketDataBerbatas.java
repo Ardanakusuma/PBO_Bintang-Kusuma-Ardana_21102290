a6ec3c9da51ab5e97ce1d0e9dd191721c997dcde
Ardanakusuma <97427520+Ardanakusuma@users.noreply.github.com>
2023-06-30 23:59:10 +0700

    Ganti file baru

--- PBO_21102290/src/com/Bintang/PBO/Pertemuan4/PaketDataBerbatas.java
+++
@@ -1,8 +0,0 @@
-package com.Bintang.PBO.Pertemuan4;
-
-public class PaketDataBerbatas extends PaketData{
-    public void infoTambah(String batas){
-        System.out.println("Batas pakai : " + batas);
-    }
-
-}
